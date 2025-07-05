d9e497d969a224c9703c087c46e6a8db88d4cd57
Hima45sagar <himsagar482@gmail.com>
2025-07-05 22:37:32 +0530

    Create dummy1.c

---
+++ dummy1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() { 
+    int x, y;
+    printf("enter number:");
+    scanf("%d", &x);
+
+    printf("enter another number:/n");
+    scanf("%d", &y);
+
+    printf("sum of %d and %d numbers %d\n",x, y, x + y);
+    printf("diffrence of %d and %d numbers %d\n", x, y, x - y);
+    printf("product of %d and %d numbers %d\n", x, y, x * y);
+    printf("division of %d and %d numbers %d\n", x, y, x / y);
+
+    
+
+
+
+
+    return 0;
+}
